2000ba92e4b5ebf60ba502267e760bc69780d487
Lailouezzz <alanlebouder@gmail.com>
2020-02-07 15:45:45 +0100

    Adding first login functions

--- BDB_www/BDB_monitoring/views.py
+++ BDB_www/BDB_monitoring/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login, logout
-from django.http import request
+from django.http import request, HttpResponse
 from django.views.generic import TemplateView
 from django.conf import settings
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
 from .models import HUMIDITY, TEMP
 
 # Create your views here.
@@ -20,6 +22,31 @@ class LoginView(TemplateView):
 
     def get(self, request, **kwargs):
         return render(request, self.template_name)
+    
+    def post(self, request, **kwargs):
+        postData = request.POST.copy()
+        username = postData.get('username', None)
+        password = postData.get('password', None)
+        response = None
+
+        if request.user.is_authenticated:
+            response = HttpResponse('already logged')
+            return response
+
+        if username == None or password == None:
+            response = HttpResponse('username and password must be specified')
+            return response
+
+        user = authenticate(username=username, password=password)
+
+        if user == None or user.is_active == False:
+            response = HttpResponse('invalid')
+            return response
+        
+        login(request, user)
+        
+        response = HttpResponse('ok')
+        return 
 
 class MonitoringView(TemplateView):
 
